893b2aad2860b90f9dd4bf6ba78bbfa7d885d1dd
uni51 <uni51@users.noreply.github.com>
2020-08-06 11:46:14 +0900

    45. Login and register page

---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container col-md-6 mt-5">
+    <h2>Login</h2>
+    <br />
+    <form>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control" placeholder="Enter email" />
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control" placeholder="Password" />
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <br />
+    <p>
+      Dont have an account?
+      <nuxt-link to="/register">Register</nuxt-link>
+    </p>
+  </div>
+</template>
