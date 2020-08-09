7c3ad82cfaab30d59a465968f6845cdd5f0a6b8c
uni51 <uni51@users.noreply.github.com>
2020-08-09 10:05:54 +0900

    60. Logout user

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -39,7 +39,7 @@
             <a class="nav-link">{{user.name}}</a>
           </li>
           <li class="nav-item">
-            <nuxt-link to="/register" class="nav-link">Logout</nuxt-link>
+            <a @click.prevent="logout" class="nav-link">Logout</a>
           </li>
         </ul>
       </template>
@@ -48,5 +48,11 @@
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    logout() {
+      this.$auth.logout();
+    },
+  },
+};
 </script>
