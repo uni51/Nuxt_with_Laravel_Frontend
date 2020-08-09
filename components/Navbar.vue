ace9b93ff4e3dcf2c4e3ad8ab6078231156127f2
uni51 <uni51@users.noreply.github.com>
2020-08-09 10:00:19 +0900

    59. Show user

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -21,15 +21,28 @@
           <nuxt-link to="/" class="nav-link">Posts</nuxt-link>
         </li>
       </ul>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <nuxt-link to="/login" class="nav-link">Login</nuxt-link>
-        </li>
-        <li class="nav-item">
-          <nuxt-link to="/register" class="nav-link">Register</nuxt-link>
-        </li>
-        {{authenticated}}
-      </ul>
+
+      <template v-if="!authenticated">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <nuxt-link to="/login" class="nav-link">Login</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/register" class="nav-link">Register</nuxt-link>
+          </li>
+        </ul>
+      </template>
+
+      <template v-if="authenticated">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link">{{user.name}}</a>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/register" class="nav-link">Logout</nuxt-link>
+          </li>
+        </ul>
+      </template>
     </div>
   </nav>
 </template>
