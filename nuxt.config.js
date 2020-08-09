f7d46b590544b36b8ab4614ee0a2a623ce28a3e9
uni51 <uni51@users.noreply.github.com>
2020-08-09 09:25:24 +0900

    56. User login

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,7 +80,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: "http://127.0.0.1:8000/api/"
+    baseURL: "http://localhost:8000/api"
   },
 
   auth: {
--- pages/login.vue
+++ pages/login.vue
@@ -2,15 +2,26 @@
   <div class="container col-md-6 mt-5">
     <h2>Login</h2>
     <br />
-    <form>
+    <form @submit.prevent="submit">
       <div class="form-group">
         <label>Email address</label>
-        <input type="email" class="form-control" placeholder="Enter email" />
+        <input
+          v-model.trim="form.email"
+          type="email"
+          class="form-control"
+          placeholder="Enter email"
+          autofocus
+        />
         <small class="form-text text-danger">Show errors here</small>
       </div>
       <div class="form-group">
         <label>Password</label>
-        <input type="password" class="form-control" placeholder="Password" />
+        <input
+          v-model.trim="form.password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+        />
         <small class="form-text text-danger">Show errors here</small>
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
@@ -22,3 +33,25 @@
     </p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    async submit() {
+      await this.$auth.loginWith("local", {
+        data: this.form
+      });
+
+      this.$router.push("/");
+    }
+  }
+};
+</script>
