3ff653b62a908edf3d9465c47b3b390de05c39f3
uni51 <uni51@users.noreply.github.com>
2020-08-09 10:30:53 +0900

    61. User registration

--- pages/register.vue
+++ pages/register.vue
@@ -2,28 +2,72 @@
   <div class="container col-md-6 mt-5">
     <h2>Register</h2>
     <br />
-    <form>
+    <form @submit.prevent="submit">
       <div class="form-group">
         <label>Full Name</label>
-        <input type="text" class="form-control" placeholder="Enter your name" />
+        <input
+          v-model.trim="form.name"
+          type="text"
+          class="form-control"
+          placeholder="Enter your name"
+          autofocus
+        />
         <small class="form-text text-danger">Show errors here</small>
       </div>
       <div class="form-group">
         <label>Email address</label>
-        <input type="email" class="form-control" placeholder="Enter email" />
+        <input
+          v-model.trim="form.email"
+          type="email"
+          class="form-control"
+          placeholder="Enter email"
+          autofocus
+        />
         <small class="form-text text-danger">Show errors here</small>
       </div>
       <div class="form-group">
         <label>Password</label>
-        <input type="password" class="form-control" placeholder="Password" />
+        <input
+          v-model.trim="form.password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+        />
         <small class="form-text text-danger">Show errors here</small>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Register</button>
     </form>
     <br />
     <p>
-      Dont have an account?
-      <nuxt-link to="/register">Register</nuxt-link>
+      Already have an account?
+      <nuxt-link to="/login">Login</nuxt-link>
     </p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async submit() {
+      await this.$axios.$post("register", this.form);
+      await this.$auth.loginWith("local", {
+        data: {
+          email: this.form.email,
+          password: this.form.password,
+        },
+      });
+      // redirect
+      this.$router.push("");
+    },
+  },
+};
+</script>
