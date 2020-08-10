d86b895bbed129e54036afe89607101654178454
uni51 <uni51@users.noreply.github.com>
2020-08-10 11:50:48 +0900

    62. Global vuex validation

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ["./plugins/mixins/user.js"],
+  plugins: ["./plugins/mixins/user.js", "./plugins/axios.js"],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
